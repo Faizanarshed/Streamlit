6bb438f8d73829ae9629e94b69f217e0391e27a9
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-04-13 04:25:33 +0500

    Update 04_ERD_Stlid.py

--- 04_ERD_Stlid.py
+++ 04_ERD_Stlid.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import seaborn as sns
-from pandas_profiling import profile_report
+from pandas_profiling import ProfileReport, profile_report
 from streamlit_pandas_profiling import st_profile_report
 
 # webapp ka title 
@@ -12,7 +12,41 @@ st.markdown('''
  ''')
 
 # how to upload a file from pc
-with st.sidebar(" Upload your Dataset (.CSV)"):
+with st.sidebar.header(" Upload your Dataset (.csv)"):
     upload_file = st.sidebar.file_uploader("Upload your file", type=['csv'])
-    df = sns.load_dataset('titanic')
-    st.sidebar.markdown("[Example CSV file]")(df)
+    delaney_with_descriptors_url = 'https://github.com/dataprofessor/data/blob/master/delaney_solubility_with_descriptors.csv'
+    df = sns.load_dataset(delaney_with_descriptors_url)
+    #df = sns.load_dataset('titanic')
+    #st.sidebar.markdown("[Example CSV file])(https://raw.github.com/dataprofessor/data/blob/master/delaney_solubility_with_descriptors.csvhttps://github.com/dataprofessor/data/blob/master/delaney_solubility_with_descriptors.csv)")
+
+#profilimg report for pandas
+if upload_file is not None:
+    @st.cache
+
+    def load_csv():
+        csv= pd.read_csv(upload_file)
+        return csv
+    df= load_csv()
+    pr= ProfileReport(df,explorative = True)
+    st.header('***input DF***')
+    st.write(df)
+    st.write('---')
+    st.header('***Profiling Report With Pandas***')
+    st_profile_report(pr)
+else:
+    st.info('Awaitng for CSV file,Upload kar bi do ap ya kam nahi Lena?')
+    if st.button('Press to use example data set'):
+        # example dataset
+        @st.cache
+
+        def load_data():
+            a = pd.DataFrame(np.random.rand(100,5),
+                                columns=['age','banana','codanics','pakistan','Ear'])
+            return a
+        df= load_data()
+        pr= profile_report(df,explorative = True)
+        st.header('***input DF***')
+        st.write(df)
+        st.write('---')
+        st.header('***Profiling Report With Pandas***')
+        st_profile_report(pr)
