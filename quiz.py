86a3d6f9bf4e65735caed57311782a5f68b1fca7
Faizanarshed <96972195+Faizanarshed@users.noreply.github.com>
2022-05-17 16:06:38 +0500

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,30 @@
+from tkinter import W
+
+
+a =40
+b =35
+c =20
+d = 10
+print(a*b/c-d)
+print(a*b/(c-d))
+def rem(a,b):
+    return a%b
+
+print(rem(3,7))
+
+a = 456
+b= 10
+c= 10
+d =10
+b = a/d
+c = a-b
+print(c)
+p =9
+w =6
+p = p+1
+w = w-1
+p = p+w
+if (p>w):
+    print(p)
+else:
+    print(w)
